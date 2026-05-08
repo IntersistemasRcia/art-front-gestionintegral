cd0c32c845a8db7495ac51cf7e3c63462080f09f
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2026-05-08 16:37:18 -0300

    Fix: se arreglo las tablas utilziadas en cta cte comercializador

--- src/data/artAPI.ts
+++ src/data/artAPI.ts
@@ -12,7 +12,7 @@ import type { FormularioVm, TipoFormulario } from "@/app/inicio/empleador/formul
 import { ParametersLocalidad, ParametersLocalidadCodigo, ParametersLocalidadNombre, DenunciaQueryParams, DenunciasApiResponse, DenunciaPostRequest, DenunciaQueryParamsID, AfiQueryParams, AfiApiResponse, PrestadorQueryParams, PrestadorResponse, DenunciaPutRequest, DenunciaPatchRequest, RefPaises, RefObraSocial, Roam, ParametersEmpleadorT, RefPrestadores, ParametersLocalidadSRT, ParametersLocalidadbyCodigo } from "@/app/inicio/denuncias/types/tDenuncias";
 import { ParametersPoliza, ParametersComercializador, OrganizadorComercializador, GrupoOrganizadorComercializador, ParametersComercializadoresAsociados } from "@/app/inicio/comercializador/polizas/types/poliza";
 import { ComercializadorPostRequest, ComercializadorPostResponse, ComercializadorPutRequest, ComercializadorPutResponse, ComercializadorDeleteParams, ComercializadorDeleteResponse, ComercializadorOrganizadoresPostRequest, ComercializadorOrganizadoresPutRequest, ComercializadorGOrganizadoresPostRequest, ComercializadorGOrganizadoresPutRequest, ComercializadorGOrganizadorById, ComercializadorById, ComercializadorOrganizadorById, SRTComercializadoresAsociadosPostRequest, SRTComercializadoresAsociadosPostResponse, SRTComercializadoresAsociadosPutRequest } from "@/app/inicio/comercializador/administracionComercializadores/types/administracionUsuarios"
-import { ParametersEmpleadorPagosComercializador, ParametersAfipTranferencia } from "@/app/inicio/comercializador/cuentaCorriente/types/cuentaCorriente";
+import { ParametersEmpleadorPagosComercializador, ParametersComercializadorPeriodoPago, ParametersAfipTranferencia } from "@/app/inicio/comercializador/cuentaCorriente/types/cuentaCorriente";
 import { CoberturaPost, CoberturaPostResponse, ParametersCobertura } from "@/app/inicio/empleador/cobertura/types/cobertura";
 import { ARCAparams, ARCAApiResponse } from "@/app/inicio/usuarios/interfaces/ARCA";
 import { EmpresaParamsID } from "@/app/inicio/usuarios/types/empresa";
@@ -2664,6 +2664,31 @@ export class ArtAPIClass extends ExternalAPI {
   //#endregion SVCC/Trabajador - Delete
   //#endregion SVCC/Trabajador
   //#endregion SVCC
+
+
+    //#region Comercializador Periodo Pago
+  readonly getComercializadorPeriodoPagoURL = (params: ParametersComercializadorPeriodoPago = {}) => {
+    return this.getURL({
+      path: "/api/ComercializadoresPeriodosPagos",
+      search: toURLSearch(params),
+    }).toString();
+  };
+  
+  getComercializadorPeriodoPago = async (params: ParametersComercializadorPeriodoPago = {}) =>
+    tokenizable.get(
+      this.getComercializadorPeriodoPagoURL(params),
+    ).then(({ data }) => data);
+
+  useGetComercializadorPeriodoPago = (params: ParametersComercializadorPeriodoPago = {}) => useSWR(
+    [this.getComercializadorPeriodoPagoURL(params), token.getToken()],
+    () => this.getComercializadorPeriodoPago(params),
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+  //#endregion
+
 
 }
 
