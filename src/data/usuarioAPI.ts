126f0220c9811f212f96e5acdd490ebc32f11d84
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2026-05-11 16:04:43 -0300

    Fix: Importacion roles interfaces

--- src/data/usuarioAPI.ts
+++ src/data/usuarioAPI.ts
@@ -6,6 +6,7 @@ import TokenConfigurator from "@/types/TokenConfigurator";
 import { toURLSearch } from "@/utils/utils";
 import IUsuarioDarDeBaja from "@/app/inicio/usuarios/interfaces/IUsuarioDarDeBajaReactivar";
 import CargoInterface from "@/app/inicio/usuarios/interfaces/CargoInterface";
+import RolesInterface from "@/app/inicio/usuarios/interfaces/RolesInterface";
 
 //#region Types
 export interface Auditable {
@@ -143,12 +144,6 @@ export type UsuarioUpdatePayload = {
   email: string;
 };
 //#region /api/Roles types
-export interface RolesInterface {
-  id: string;
-  nombre: string;
-  nombreNormalizado: string;
-  esRolHijo: boolean;
-}
 //#endregion /api/Roles types
 //#region /api/Tablas types
 export interface Campo extends Auditable {
